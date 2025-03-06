6a0298869c3b31f4458411582a3ce3fbcee2c613
aleksi-svaridze <aleksi.svaridze08b@geolab.edu.ge>
2025-03-06 11:15:02 +0400

    Change Heros layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,17 @@ function Hero() {
   return (
     <div className="mt-10 mb-20">
       <div
-        className="h-96 rounded-xl flex items-center justify-end bg-top bg-no-repeat bg-cover pr-10 shadow-[0_0_20px_5px_rgba(0,0,0,0.5)]"
+        className="h-[500px] md:h-96 rounded-xl flex items-center justify-center md:justify-end bg-left md:bg-top bg-no-repeat bg-cover md:pr-10 shadow-[0_0_20px_5px_rgba(0,0,0,0.5)]"
         style={{
           backgroundImage: `url(${HeroBg})`,
         }}
       >
-        <div className="text-white max-w-1/2">
-          <h1 className="text-4xl font-bold">
+        <div className="text-white max-w-1/2 text-center md:text-left hidden md:block">
+          <h1 className="text-xl md:text-4xl font-bold">
             Spider-man versions
             <br /> for all platforms
           </h1>
-          <h3 className="text-xl font-medium mt-5">
+          <h3 className="text-base md:text-xl font-medium mt-5">
             Some descrih4tion here...Some description here...Some description
             here...Some description here...
           </h3>
