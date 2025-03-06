6474f726ab2eebfb8d8eb133ab0e2801b08c52d1
aleksi-svaridze <aleksi.svaridze08b@geolab.edu.ge>
2025-03-06 10:56:04 +0400

    Change card view

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router";
 
-export const CarouselCard = ({ title, desc, src }) => {
+export const CarouselCard = ({ title, src }) => {
   return (
-    <article className="rounded-xl overflow-hidden shadow-[0_0_20px_5px_rgba(0,0,0,0.5)]">
-      <img className="w-full" src={src} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <p>{desc}</p>
-        <Link to={""}>View</Link>
+    <article className="overflow-hidden shadow-[0_0_20px_5px_rgba(0,0,0,0.5)]">
+      <div
+        style={{ backgroundImage: `url(${src})` }}
+        className="w-full h-96 md:h-60 lg:h-80 xl:h-[400px] 2xl:h-[490px] bg-cover bg-center bg-no-repeat"
+      ></div>
+      <div className="py-5 px-4">
+        <h2 className="font-semibold text-xl md:text-base xl:text-2xl">
+          {title}
+        </h2>
+        <Link to={""} className="text-lg border-2 mt-5 inline-block px-3 py-1">
+          Get Game
+        </Link>
       </div>
     </article>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,16 +8,31 @@ import SpiderMan2 from "../assets/images/desktop/spider-man-2.jpg";
 
 function HomePage() {
   const [pcGames] = useState([
-    { id: 1, src: MilesMorales, title: "title 1", desc: "desc 1" },
-    { id: 2, src: SpiderMan2, title: "title 2", desc: "desc 2" },
-    { id: 3, src: Remastered, title: "title 3", desc: "desc 3" },
+    {
+      id: 1,
+      src: Remastered,
+      title: "Marvel's Spider-Man Remastered Standard - PC",
+      desc: "desc 1",
+    },
+    {
+      id: 2,
+      src: MilesMorales,
+      title: "Marvel's Spider-Man: Miles Morales Standard - PC",
+      desc: "desc 2",
+    },
+    {
+      id: 3,
+      src: SpiderMan2,
+      title: "Marvel's Spider-Man 2 Digital Deluxe Edition - PC",
+      desc: "desc 3",
+    },
   ]);
   return (
     <div className="h-full">
       <Hero />
-      <div className="flex flex-col gap-y-5">
+      <div className="flex flex-col gap-y-5 mt-20">
         <div className="flex items-center justify-between text-white">
-          <h2 className="text-3xl">Spider-Man for PCs</h2>
+          <h2 className="text-xl lg:text-3xl">Spider-Man for PCs</h2>
           <Link
             to={"/pc"}
             className="text-xs border-[1px] py-1 px-2 rounded-md"
@@ -25,14 +40,26 @@ function HomePage() {
             View All
           </Link>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-between gap-x-2 md:gap-x-5 lg:gap-x-10">
+        <div className="flex flex-col gap-y-10 md:gap-y-0 md:flex-row items-center justify-between gap-x-2 md:gap-x-5 lg:gap-x-10">
           {pcGames.map((item) => (
-            <CarouselCard
-              src={item.src}
-              title={item.title}
-              desc={item.desc}
-              key={item.id}
-            />
+            <CarouselCard src={item.src} title={item.title} key={item.id} />
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-5 mt-20">
+        <div className="flex items-center justify-between text-white">
+          <h2 className="text-xl lg:text-3xl">Spider-Man for Xbox</h2>
+          <Link
+            to={"/pc"}
+            className="text-xs border-[1px] py-1 px-2 rounded-md"
+          >
+            View All
+          </Link>
+        </div>
+        <div className="flex flex-col gap-y-10 md:gap-y-0 md:flex-row items-center justify-between gap-x-2 md:gap-x-5 lg:gap-x-10">
+          {pcGames.map((item) => (
+            <CarouselCard src={item.src} title={item.title} key={item.id} />
           ))}
         </div>
       </div>
